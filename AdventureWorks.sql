f67e782d76054039a0cf915fc47ddf95e2e35195
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-12 08:11:40 +0800

    Update AdventureWorks.sql

--- AdventureWorks.sql
+++ AdventureWorks.sql
@@ -1,5 +1,15 @@
--- dsadsa
-SELECT
-  *
-FROM
-  ggg
+-- 1. Retrieve the first 5 products from the Product table
+SELECT 
+    TOP 5 *
+FROM 
+    DimProduct
+
+-- 2.  Get the top 10 employees from the Employee table along with their job titles.
+SELECT 
+    TOP 10 EmployeeKey, 
+    FirstName, 
+    MiddleName, 
+    LastName, 
+    Title
+FROM 
+    DimEmployee
