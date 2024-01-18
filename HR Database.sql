d765c6abe172dbb2f2f04aac11b0b6ddd1981fcf
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-18 09:11:03 +0800

    Update HR Database.sql

--- HR Database.sql
+++ HR Database.sql
@@ -69,3 +69,149 @@ INTO
 	DimDepartment
 FROM
 	HRDataset_v14
+
+-- PerformaceScore | Normalization |
+	-- Data Cleaning
+UPDATE HRDataset_v14
+SET PerfScoreID = 2
+WHERE PerformanceScore = 'PIP'
+
+UPDATE HRDataset_v14
+SET PerformanceScore = 'Needs Improvement'
+WHERE PerfScoreID = 2
+
+UPDATE HRDataset_v14
+SET PerfScoreID = 1
+WHERE PerformanceScore = 'Fully Meets'
+
+UPDATE HRDataset_v14
+SET PerfScoreID = 3
+WHERE PerformanceScore ='Exceeds'
+	-- Transfering the data into DimPerformanceScore Table
+Select 
+	DISTINCT(PerfScoreID),
+	PerformanceScore
+INTO
+	DimPerformanceScore
+From 
+	HRDataset_v14
+
+-- DimPosition | Normalization |
+	-- Data Cleaning
+UPDATE HRDataset_v14
+SET PositionID = 24
+WHERE Position = 'Software Engineer'
+
+UPDATE HRDataset_v14
+SET PositionID = 18
+WHERE Position = 'Production Manager'
+
+UPDATE HRDataset_v14
+SET Position = 'IT Manager'
+WHERE PositionID = 13
+-- Transfering the data into DimPosition Table
+SELECT
+	DISTINCT(PositionID),
+	Position
+INTO
+	DimPosition
+FROM
+	HRDataset_v14
+
+-- DimMaritalStatus | Normalization |
+	-- MaritalStatus Data is clean.
+	-- Transfering the data into DimMaritalStatus Table
+SELECT
+	DISTINCT(MaritalStatusID),
+	MaritalDesc
+INTO
+	DimMaritalStatus
+FROM
+	HRDataset_v14
+
+-- DimManager
+	-- Data Cleaning
+UPDATE HRDataset_v14
+SET ManagerID = 39
+WHERE ManagerName = 'Webster Butler'
+
+UPDATE HRDataset_v14
+SET ManagerID = 22
+WHERE ManagerName = 'Michael Albert'
+
+UPDATE HRDataset_v14
+SET ManagerID = 1
+WHERE ManagerName = 'Brandon R. LeBlanc'
+	-- Transfering the data into DimManager Table
+SELECT
+	DISTINCT(ManagerID),
+	ManagerName
+INTO
+	DimManager
+FROM
+	HRDataset_v14
+
+----- EmpSatisfaction | Normalization |
+	-- Employment Satisfaction has missing information, so I reasearch on what is the corresponing value of each number and improvise the data base on data that i researched.
+	-- Transfering the one column to its designated Table
+SELECT
+	DISTINCT(EmpSatisfaction) AS EmpSatisfactionID
+INTO
+	DimEmpSatisfaction
+FROM
+	HRDataset_v14
+	-- Adding EmpSatisfaction column to for the the value of the ID
+ALTER TABLE DimEmpSatisfaction
+ADD EmpSatisfaction NVARCHAR(20)
+	-- This is the data I research that I will put on my project
+	---- Add customize value from google, Add column
+		--1	Very Dissatisfied
+		--2	Dissatisfied
+		--3	Nuetral
+		--4	Satisfied
+		--5	Very Satisfied
+
+	-- Entering the data
+UPDATE DimEmpSatisfaction
+SET EmpSatisfaction = 'Very Dissatisfied'
+WHERE EmpSatisfactionID = 1
+
+UPDATE DimEmpSatisfaction
+SET EmpSatisfaction = 'Dissatisfied'
+WHERE EmpSatisfactionID = 2
+
+UPDATE DimEmpSatisfaction
+SET EmpSatisfaction = 'Nuetral'
+WHERE EmpSatisfactionID = 3
+
+UPDATE DimEmpSatisfaction
+SET EmpSatisfaction = 'Satisfied'
+WHERE EmpSatisfactionID = 4
+
+UPDATE DimEmpSatisfaction
+SET EmpSatisfaction = 'Very Satisfied'
+WHERE EmpSatisfactionID = 5
+
+-- DimEmployee | Normalization |
+	-- Transfering the choosen column to its designated table which is DimEmployee
+SELECT 
+	 EmpID,
+	 Employee_Name AS EmployeeName,
+	 Salary,
+	 State,
+	 Zip,
+	 DOB AS DateOFBirth,
+	 CitizenDesc,
+	 RaceDesc,
+	 DateofHire,
+	 DateofTermination,
+	 TermReason,
+	 RecruitmentSource,
+	 SpecialProjectsCount,
+	 LastPerformanceReview_Date,
+	 DaysLateLast30,
+	 Absences
+INTO
+	DimEmployee
+FROM 
+	HRDataset_v14
