68abf9493e226c4a29a062b97b27ff739bc573a6
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-18 18:15:40 +0800

    Update HR Database.sql

--- HR Database.sql
+++ HR Database.sql
@@ -23,214 +23,209 @@
 
 	--  EmploymentStatus | Normalization |
 	-- The column for DimEmploymentStatus contains duplicate values in its employment status ID and employment status, and it is necessary to replace it with its designated value.
-	UPDATE HRDataset_v14
-	SET EmpStatusID = 4
-	WHERE EmploymentStatus = 'Terminated for Cause'
-	
-	UPDATE HRDataset_v14
-	SET EmpStatusID = 1
-	WHERE EmploymentStatus ='Active'
-	
-		 Organizing
-	UPDATE HRDataset_v14
-	SET EmpStatusID = 2
-	WHERE EmploymentStatus = 'Terminated for Cause'
-	
-	UPDATE HRDataset_v14
-	SET EmpStatusID = 3
-	WHERE EmploymentStatus = 'Voluntarily Terminated'
+		UPDATE HRDataset_v14
+		SET EmpStatusID = 4
+		WHERE EmploymentStatus = 'Terminated for Cause'
+		
+		UPDATE HRDataset_v14
+		SET EmpStatusID = 1
+		WHERE EmploymentStatus ='Active'
+		
+			 Organizing
+		UPDATE HRDataset_v14
+		SET EmpStatusID = 2
+		WHERE EmploymentStatus = 'Terminated for Cause'
+		
+		UPDATE HRDataset_v14
+		SET EmpStatusID = 3
+		WHERE EmploymentStatus = 'Voluntarily Terminated'
         --Transfering into DimEmploymentStatus Table
-	SELECT
-		DISTINCT(EmpStatusID),
-		EmploymentStatus
-	INTO
-		DimEmploymentStatus
-	FROM
-		HRDataset_v14
+		SELECT
+			DISTINCT(EmpStatusID),
+			EmploymentStatus
+		INTO
+			DimEmploymentStatus
+		FROM
+			HRDataset_v14
 
---  Department | Normalization |
-        -- Some Id and department is duplcate, so need to clean.
-
-UPDATE HRDataset_v14
-SET DeptID = 4
-WHERE Department = 'Software Engineering'
-
-UPDATE HRDataset_v14
-SET DeptID = 5
-WHERE Department = 'Production'
-
+	--  Department | Normalization |
+        -- The column for DimDepartment contains duplicate values, and it is necessary to replace it with its designated value.
+		UPDATE HRDataset_v14
+		SET DeptID = 4
+		WHERE Department = 'Software Engineering'
+		
+		UPDATE HRDataset_v14
+		SET DeptID = 5
+		WHERE Department = 'Production'
         --Transfer the data into DimDepartment
-SELECT
-	DISTINCT(DeptID),
-	Department
-INTO
-	DimDepartment
-FROM
-	HRDataset_v14
+		SELECT
+			DISTINCT(DeptID),
+			Department
+		INTO
+			DimDepartment
+		FROM
+			HRDataset_v14
 
--- PerformaceScore | Normalization |
-	-- Data Cleaning
-UPDATE HRDataset_v14
-SET PerfScoreID = 2
-WHERE PerformanceScore = 'PIP'
-
-UPDATE HRDataset_v14
-SET PerformanceScore = 'Needs Improvement'
-WHERE PerfScoreID = 2
-
-UPDATE HRDataset_v14
-SET PerfScoreID = 1
-WHERE PerformanceScore = 'Fully Meets'
-
-UPDATE HRDataset_v14
-SET PerfScoreID = 3
-WHERE PerformanceScore ='Exceeds'
+	-- PerformaceScore | Normalization |
+	-- The column for DimPerformaceScore contains duplicate values, and it is necessary to replace it with its designated value.
+		UPDATE HRDataset_v14
+		SET PerfScoreID = 2
+		WHERE PerformanceScore = 'PIP'
+		
+		UPDATE HRDataset_v14
+		SET PerformanceScore = 'Needs Improvement'
+		WHERE PerfScoreID = 2
+		
+		UPDATE HRDataset_v14
+		SET PerfScoreID = 1
+		WHERE PerformanceScore = 'Fully Meets'
+		
+		UPDATE HRDataset_v14
+		SET PerfScoreID = 3
+		WHERE PerformanceScore ='Exceeds'
 	-- Transfering the data into DimPerformanceScore Table
-Select 
-	DISTINCT(PerfScoreID),
-	PerformanceScore
-INTO
-	DimPerformanceScore
-From 
-	HRDataset_v14
+		Select 
+			DISTINCT(PerfScoreID),
+			PerformanceScore
+		INTO
+			DimPerformanceScore
+		From 
+			HRDataset_v14
 
--- DimPosition | Normalization |
-	-- Data Cleaning
-UPDATE HRDataset_v14
-SET PositionID = 24
-WHERE Position = 'Software Engineer'
+	-- DimPosition | Normalization |
+	-- The column for DimPosition contains duplicate values, and it is necessary to replace it with its designated value.
+		UPDATE HRDataset_v14
+		SET PositionID = 24
+		WHERE Position = 'Software Engineer'
+		
+		UPDATE HRDataset_v14
+		SET PositionID = 18
+		WHERE Position = 'Production Manager'
+		
+		UPDATE HRDataset_v14
+		SET Position = 'IT Manager'
+		WHERE PositionID = 13
+	-- Transfering the data into DimPosition Table
+		SELECT
+			DISTINCT(PositionID),
+			Position
+		INTO
+			DimPosition
+		FROM
+			HRDataset_v14
 
-UPDATE HRDataset_v14
-SET PositionID = 18
-WHERE Position = 'Production Manager'
-
-UPDATE HRDataset_v14
-SET Position = 'IT Manager'
-WHERE PositionID = 13
--- Transfering the data into DimPosition Table
-SELECT
-	DISTINCT(PositionID),
-	Position
-INTO
-	DimPosition
-FROM
-	HRDataset_v14
-
--- DimMaritalStatus | Normalization |
-	-- MaritalStatus Data is clean.
+	-- DimMaritalStatus | Normalization |
 	-- Transfering the data into DimMaritalStatus Table
-SELECT
-	DISTINCT(MaritalStatusID),
-	MaritalDesc
-INTO
-	DimMaritalStatus
-FROM
-	HRDataset_v14
+		SELECT
+			DISTINCT(MaritalStatusID),
+			MaritalDesc
+		INTO
+			DimMaritalStatus
+		FROM
+			HRDataset_v14
 
--- DimManager
-	-- Data Cleaning
-UPDATE HRDataset_v14
-SET ManagerID = 39
-WHERE ManagerName = 'Webster Butler'
-
-UPDATE HRDataset_v14
-SET ManagerID = 22
-WHERE ManagerName = 'Michael Albert'
-
-UPDATE HRDataset_v14
-SET ManagerID = 1
-WHERE ManagerName = 'Brandon R. LeBlanc'
+	-- DimManager | Normalization |
+	-- The column for DimManager contains duplicate values, and it is necessary to replace it with its designated value.
+		UPDATE HRDataset_v14
+		SET ManagerID = 39
+		WHERE ManagerName = 'Webster Butler'
+		
+		UPDATE HRDataset_v14
+		SET ManagerID = 22
+		WHERE ManagerName = 'Michael Albert'
+		
+		UPDATE HRDataset_v14
+		SET ManagerID = 1
+		WHERE ManagerName = 'Brandon R. LeBlanc'
 	-- Transfering the data into DimManager Table
-SELECT
-	DISTINCT(ManagerID),
-	ManagerName
-INTO
-	DimManager
-FROM
-	HRDataset_v14
+		SELECT
+			DISTINCT(ManagerID),
+			ManagerName
+		INTO
+			DimManager
+		FROM
+			HRDataset_v14
 
------ EmpSatisfaction | Normalization |
-	-- Employment Satisfaction has missing information, so I reasearch on what is the corresponing value of each number and improvise the data base on data that i researched.
+	-- EmpSatisfaction | Normalization |
+	--The table has a missing column, which is the value of the employment satisfaction ID. So I improvise the data by researching its value, and here's what I researched:
+	--1   Very Dissatisfied
+	--2   Dissatisfied
+	--3   Nuetral
+	--4   Satisfied
+	--5   Very Satisfied
+			
 	-- Transfering the one column to its designated Table
-SELECT
-	DISTINCT(EmpSatisfaction) AS EmpSatisfactionID
-INTO
-	DimEmpSatisfaction
-FROM
-	HRDataset_v14
+		SELECT
+			DISTINCT(EmpSatisfaction) AS EmpSatisfactionID
+		INTO
+			DimEmpSatisfaction
+		FROM
+			HRDataset_v14
 	-- Adding EmpSatisfaction column to for the the value of the ID
-ALTER TABLE DimEmpSatisfaction
-ADD EmpSatisfaction NVARCHAR(20)
-	-- This is the data I research that I will put on my project
-	---- Add customize value from google, Add column
-		--1	Very Dissatisfied
-		--2	Dissatisfied
-		--3	Nuetral
-		--4	Satisfied
-		--5	Very Satisfied
-
+		ALTER TABLE DimEmpSatisfaction
+		ADD EmpSatisfaction NVARCHAR(20)
 	-- Entering the data
-UPDATE DimEmpSatisfaction
-SET EmpSatisfaction = 'Very Dissatisfied'
-WHERE EmpSatisfactionID = 1
+		UPDATE DimEmpSatisfaction
+		SET EmpSatisfaction = 'Very Dissatisfied'
+		WHERE EmpSatisfactionID = 1
 
-UPDATE DimEmpSatisfaction
-SET EmpSatisfaction = 'Dissatisfied'
-WHERE EmpSatisfactionID = 2
+		UPDATE DimEmpSatisfaction
+		SET EmpSatisfaction = 'Dissatisfied'
+		WHERE EmpSatisfactionID = 2
 
-UPDATE DimEmpSatisfaction
-SET EmpSatisfaction = 'Nuetral'
-WHERE EmpSatisfactionID = 3
+		UPDATE DimEmpSatisfaction
+		SET EmpSatisfaction = 'Nuetral'
+		WHERE EmpSatisfactionID = 3
 
-UPDATE DimEmpSatisfaction
-SET EmpSatisfaction = 'Satisfied'
-WHERE EmpSatisfactionID = 4
+		UPDATE DimEmpSatisfaction
+		SET EmpSatisfaction = 'Satisfied'
+		WHERE EmpSatisfactionID = 4
 
-UPDATE DimEmpSatisfaction
-SET EmpSatisfaction = 'Very Satisfied'
-WHERE EmpSatisfactionID = 5
+		UPDATE DimEmpSatisfaction
+		SET EmpSatisfaction = 'Very Satisfied'
+		WHERE EmpSatisfactionID = 5
 
--- Employee | Normalization |
+	-- Employee | Normalization |
 	-- Transfering the choosen column to its designated table which is DimEmployee
-SELECT 
-	 EmpID,
-	 Employee_Name AS EmployeeName,
-	 Salary,
-	 State,
-	 Zip,
-	 DOB AS DateOFBirth,
-	 CitizenDesc,
-	 RaceDesc,
-	 DateofHire,
-	 DateofTermination,
-	 TermReason,
-	 RecruitmentSource,
-	 SpecialProjectsCount,
-	 LastPerformanceReview_Date,
-	 DaysLateLast30,
-	 Absences
-INTO
-	DimEmployee
-FROM 
-	HRDataset_v14
+		SELECT 
+			 EmpID,
+			 Employee_Name AS EmployeeName,
+			 Salary,
+			 State,
+			 Zip,
+			 DOB AS DateOFBirth,
+			 CitizenDesc,
+			 RaceDesc,
+			 DateofHire,
+			 DateofTermination,
+			 TermReason,
+			 RecruitmentSource,
+			 SpecialProjectsCount,
+			 LastPerformanceReview_Date,
+			 DaysLateLast30,
+			 Absences
+		INTO
+			DimEmployee
+		FROM 
+			HRDataset_v14
 
---HRRecord | Normalization |
+	--HRRecord | Normalization |
 	-- Transfering the data into FactHRRecord Table
-SELECT
-	EmpID,
-	MarriedID,
-	MaritalStatusID,
-	GenderID,
-	EmpStatusID,
-	DeptID,
-	PerfScoreID,
-	PositionID,
-	ManagerID,
-	EmpSatisfaction AS EmpSatisfactionID
-INTO
-	FactHRRecord
-FROM 
-	HRDataset_v14
+		SELECT
+			EmpID,
+			MarriedID,
+			MaritalStatusID,
+			GenderID,
+			EmpStatusID,
+			DeptID,
+			PerfScoreID,
+			PositionID,
+			ManagerID,
+			EmpSatisfaction AS EmpSatisfactionID
+		INTO
+			FactHRRecord
+		FROM 
+			HRDataset_v14
 
 
 ----------------------------- Changing Data types based on the data inputs
