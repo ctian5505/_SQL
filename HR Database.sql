44253e25c58ae18b4c2e22bdf0b5671389ea64d2
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-18 08:57:09 +0800

    Update HR Database.sql

--- HR Database.sql
+++ HR Database.sql
@@ -4,3 +4,20 @@
   -- Adding Constraint
 
 -- Querying
+
+--------------------------------------------------------------
+-- Create Database
+-- Import the file | File Link:
+    -- Raw data is nasa isang table lang which is not a good practice, so that I need to normalize it, and put it to their desinated tables
+
+---------------------* Data Cleaning & Normalization
+
+--  DimGender | Normalization |
+        --Transfering the data into DimGender Table
+SELECT
+  	DISTINCT(GenderID),
+  	Sex
+INTO
+  	DimGender
+FROM
+  	HRDataset_v14
