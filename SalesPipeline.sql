c710142b573e239376ae29adda9112a7a6d8ce24
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-30 10:30:56 +0800

    Update SalesPipeline.sql

--- SalesPipeline.sql
+++ SalesPipeline.sql
@@ -205,3 +205,211 @@ WHERE
 
 --SELECT * FROM Sales
 
+-----------
+Practice Pipeline
+
+CREATE PROCEDURE Test
+AS
+BEGIN
+	--- Droping Temporary Table if Existed
+	DROP TABLE #TempTable
+	--- Creating Temporary Table
+	CREATE TABLE #TempTable(
+	Transaction_ID INT NOT NULL,
+	Order_ID NVARCHAR(50) NOT NULL,
+	Order_Date DATE NOT NULL,
+	Ship_Date DATE NOT NULL,
+	Ship_Mode NVARCHAR(50) NOT NULL,
+	Customer_ID NVARCHAR(50) NOT NULL,
+	Customer_Name NVARCHAR(100) NOT NULL,
+	Segment NVARCHAR(50) NOT NULL,
+	Country NVARCHAR(50) NOT NULL,
+	City NVARCHAR(50) NOT NULL,
+	State NVARCHAR(50) NOT NULL,
+	Postal_Code INT NOT NULL,
+	Region NVARCHAR(50) NOT NULL,
+	Product_ID NVARCHAR(50) NOT NULL,
+	Category NVARCHAR(100) NOT NULL,
+	Sub_Category NVARCHAR(100) NOT NULL,
+	Product_Name NVARCHAR(250) NOT NULL,
+	Price MONEY NOT NULL,
+	Sales MONEY NOT NULL,
+	Quantity INT NOT NULL,
+	)
+
+	INSERT INTO #TempTable(
+		Transaction_ID,
+		Order_ID,
+		Order_Date,
+		Ship_Date,
+		Ship_Mode,
+		Customer_ID,
+		Customer_Name,
+		Segment,
+		Country,
+		City,
+		State,
+		Postal_Code,
+		Region,
+		Product_ID,
+		Category,
+		Sub_Category,
+		Product_Name,
+		Price,
+		Sales,
+		Quantity)
+	SELECT * FROM Superstore
+
+	-- Inserting into Category (Pipeline)
+	INSERT INTO Category (
+		Category
+		)
+	SELECT 
+		DISTINCT(s.Category) 
+	FROM 
+		#TempTable AS s
+	LEFT JOIN 
+		Category AS c
+	ON	
+		s.Category = c.Category
+	WHERE 
+		c.Category IS NULL
+
+	--Inserting into SubCatogery (Pipeline)
+	INSERT INTO SubCategory(
+		Subcategory, 
+		CategoryID
+		)
+	SELECT 
+		DISTINCT(s.Sub_Category), 
+		c.CategoryID 
+	FROM 
+			#TempTable AS s
+	LEFT JOIN 
+		Category as c
+	ON 
+		s.Category = c.Category
+	LEFT JOIN 
+		SubCategory AS sc 
+	ON
+		s.Sub_Category = sc.SubCategory
+	WHERE 
+		sc.SubCategory IS NULL
+
+	--Inserting into Product (Pipeline)
+	INSERT INTO Product(
+		ProductID, 
+		ProductName, 
+		SubCategoryID, 
+		Price
+		)
+	SELECT
+		DISTINCT(s.Product_ID),
+		s.Product_Name, 
+		sc.SubCategoryID,
+		s.Price 
+	FROM 
+		#TempTable as s
+	LEFT JOIN
+		SubCategory AS sc
+	ON 
+		s.Sub_Category = sc.SubCategory
+	LEFT JOIN
+		Product AS p
+	ON
+		s.Product_ID = p.ProductID
+	WHERE p.ProductID IS NULL
+
+	-- Creating Segment
+	INSERT INTO Segment(
+		Segment
+		)
+	SELECT 
+		DISTINCT(s.Segment)
+	FROM 
+		#TempTable AS s
+	LEFT JOIN
+		Segment AS sg
+	ON
+		s.Segment = sg.Segment
+	WHERE
+		sg.Segment IS NULL
+
+	-- Creating ShipMode
+	INSERT INTO ShipMode(ShipMode)
+	SELECT 
+		DISTINCT(s.Ship_Mode)
+	FROM 
+		#TempTable AS s
+	LEFT JOIN
+		ShipMode AS sm
+	ON
+		s.Ship_Mode = sm.ShipMode
+	WHERE 
+		sm.ShipMode IS NULL
+
+	--Creating Customer
+	INSERT INTO Customer(
+		CustomerID,
+		CustomerName,
+		City,State,
+		Region,
+		Country,
+		PostalCode)
+	SELECT 
+		DISTINCT(s.Customer_ID),
+		s.Customer_Name,
+		s.City,
+		s.State,
+		s.Region,
+		s.Country,
+		s.Postal_Code
+	FROM 
+		#TempTable AS s
+	LEFT JOIN 
+		Customer AS c
+	ON
+		s.Customer_ID = c.CustomerID
+	WHERE
+		c.CustomerID IS NULL
+
+	-- Creating Sales
+	INSERT INTO Sales(TransactionID,OrderID,OrderDate,ShipDate,ShipModeID,CustomerID, SegmentID, ProdcuctID,Quantity)
+	SELECT 
+		s.Transaction_ID,
+		s.Order_ID,
+		s.Order_Date,
+		s.Ship_Date,
+		sm.ShipModeID,
+		c.CustomerID,
+		seg.SegmentID,
+		prod.ProductID,
+		s.Quantity
+	FROM 
+		Source AS s
+	LEFT JOIN 
+		ShipMode AS sm
+	ON
+		s.Ship_Mode = sm.ShipMode
+	LEFT JOIN
+		Customer AS c
+	ON
+		s.Customer_ID = c.CustomerID
+	LEFT JOIN
+		Segment AS seg
+	ON
+		s.Segment = seg.Segment
+	LEFT JOIN
+		Product AS prod
+	ON
+		s.Product_ID =prod.ProductID
+	LEFT JOIN
+		Sales AS sales
+	ON
+		s.Transaction_ID = sales.TransactionID
+	WHERE
+		sales.TransactionID IS NULL
+
+	--- Droping Temporary Table
+	DROP TABLE #TempTable
+END
