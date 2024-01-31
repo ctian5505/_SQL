ae5689aab8ff838722a98594bfde91c2021cd77e
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2024-01-31 08:10:11 +0800

    Update SalesPipeline.sql

--- SalesPipeline.sql
+++ SalesPipeline.sql
@@ -1,7 +1,29 @@
--- Datasets
+-- Datasets : https://drive.google.com/file/d/15Jko-HW6HSDAcWCEoKUdZC9wSghxjk9M/view?usp=sharing
 
------------Creating Table
---Creating Sales Table
+-- I created a stored procedure that, when executed, will automatically transfer the data into its designated table from raw data. This stored procedure will also not input duplicate values. This is just the first version of the stored procedure; more improvements will come.
+
+-- This data is connected to Microsoft PowerBI and is still under construction.
+
+-- Steps if you want to try it 
+
+-- Here are the steps if you want to try it.
+	-- Download the dataset
+	-- Create a Database on MSSQL Server with a name of SalesDB (Code Below)
+	-- Import the CSV data into SalesDB
+	-- Create table (Code Below)
+	-- Add Constraint | Primary key & Foreign Key | (Code Below)
+	-- Create the Stored Procedure (Code Below)
+		-- This stored procedure will create a temporary table based on the data that we import, then it will check if there are new unique entries, and it will transfer those new data into its designated table. After that, the temporary table will be deleted.
+	-- Executed the Stored Procedure by Querying "EXEC  Test"
+	-- After that, you can drop the table that we imported (SuperSale), and if we want to import new data, just import the data set and query "EXEC Test" and repeat.
+
+
+
+-- Creating Database
+CREATE DATABASE SalesDB
+
+--Creating Table
+	--Creating Sales Table
 CREATE TABLE Sales(
 TransactionID INT NOT NULL,
 OrderID NVARCHAR(50) NOT NULL,
@@ -13,9 +35,8 @@ SegmentID INT NOT NULL,
 ProductID NVARCHAR(50) NOT NULL,
 Quantity INT NOT NULL
 )
-
-
---Creating Customer Table
+	
+	--Creating Customer Table
 CREATE TABLE Customer(
 CustomerID NVARCHAR(50) NOT NULL,
 CustomerName NVARCHAR(100) NOT NULL,
@@ -25,118 +46,114 @@ Region NVARCHAR(50) NOT NULL,
 Country NVARCHAR(50) NOT NULL,
 PostalCode INT NOT NULL
 )
-
---Creating ShipMode Table
+	
+	--Creating ShipMode Table
 CREATE TABLE ShipMode (
 ShipModeID INT IDENTITY(1,1) NOT NULL,
 ShipMode NVARCHAR(50)
 )
-
-
---Creating Segment Table
+	
+	--Creating Segment Table
 CREATE TABLE Segment(
 SegmentID INT IDENTITY(1,1) NOT NULL,
 Segment NVARCHAR(50)
-)
-
---Creating Product Table
+)	
+	
+	--Creating Product Table
 CREATE TABLE Product(
 ProductID NVARCHAR(50) NOT NULL,
 ProductName NVARCHAR(250) NOT NULL,
 SubCategoryID INT NOT NULL,
 Price MONEY
 )
-
---Creating SubCategory Table
+	
+	--Creating SubCategory Table
 CREATE TABLE SubCategory(
 SubCategoryID INT IDENTITY(1,1) NOT NULL,
 SubCategory NVARCHAR(100) NOT NULL,
 CategoryID INT NOT NULL
 )
 	
-	
-
---Creating Category Table
+	--Creating Category Table
 CREATE TABLE Category (
 CategoryID INT IDENTITY(1,1) NOT NULL,
 Category NVARCHAR(100) NOT NULL
 )
 
----------- Adding Constraint
---- Adding Primary Key Constrain on Customer..CustomerID
+-- Adding Constraint
+	-- Adding Primary Key Constrain on Customer..CustomerID
 ALTER TABLE Customer
 ADD CONSTRAINT PK_CustomerID
 PRIMARY KEY(CustomerID)
-
---- Adding Primary Key Constrain on ShipMode..ShipModeID
+	
+	-- Adding Primary Key Constrain on ShipMode..ShipModeID
 ALTER TABLE ShipMode
 ADD CONSTRAINT PK_ShipModeID
 PRIMARY KEY(ShipModeID)
 
---- Adding Primary Key Constrain on Segment..SegmentID
+	-- Adding Primary Key Constrain on Segment..SegmentID
 ALTER TABLE Segment
 ADD CONSTRAINT PK_SegmentID
 PRIMARY KEY(SegmentID)
 
---- Adding Primary Key Constrain on Product..ProductID
+	-- Adding Primary Key Constrain on Product..ProductID
 ALTER TABLE Product
 ADD CONSTRAINT PK_ProductID
 PRIMARY KEY(ProductID)
 
---- Adding Primary Key Constrain on SubCategory..SubCategoryID
+	-- Adding Primary Key Constrain on SubCategory..SubCategoryID
 ALTER TABLE SubCategory
 ADD CONSTRAINT PK_SubCategoryID
 PRIMARY KEY(SubCategoryID)
 
---- Adding Primary Key Constrain on Category..CategoryID
+	-- Adding Primary Key Constrain on Category..CategoryID
 ALTER TABLE Category
 ADD CONSTRAINT PK_CategoryID
 PRIMARY KEY(CategoryID)
 
---- Adding Foreign Key Constrain on SubCategory..CategoryID
+	-- Adding Foreign Key Constrain on SubCategory..CategoryID
 ALTER TABLE SubCategory
 ADD CONSTRAINT FK_CategoryKey
 FOREIGN KEY(CategoryID)
 REFERENCES Category(CategoryID)
 
---- Adding Foreign Key Constrain on Product..SubCategoryID
+	-- Adding Foreign Key Constrain on Product..SubCategoryID
 ALTER TABLE Product
 ADD CONSTRAINT FK_SubCategoryID
 FOREIGN KEY (SubCategoryID)
 REFERENCES SubCategory(SubCategoryID)
 
---- Adding Foreign Key Constrain on Sales..ProductID
+	-- Adding Foreign Key Constrain on Sales..ProductID
 ALTER TABLE Sales
 ADD CONSTRAINT FK_ProductID
 FOREIGN KEY(ProductID) 
 REFERENCES Product(ProductID)	
 	
---- Adding Foreign Key Constrain on Sales..SegmentID
+	-- Adding Foreign Key Constrain on Sales..SegmentID
 ALTER TABLE Sales
 ADD CONSTRAINT FK_SegmentID
 FOREIGN KEY(SegmentID)
 REFERENCES Segment(SegmentID)
 
---- Adding Foreign Key Constrain on ShipMode..FK_ShipmodeID
+	-- Adding Foreign Key Constrain on ShipMode..FK_ShipmodeID
 ALTER TABLE Sales
 ADD CONSTRAINT FK_ShipmodeID
 FOREIGN KEY(ShipModeID)
 REFERENCES ShipMode(ShipModeID)
 
---- Adding Foreign Key Constrain on Sales..CustomerID
+	-- Adding Foreign Key Constrain on Sales..CustomerID
 ALTER TABLE Sales
 ADD CONSTRAINT FK_CustomerID
 FOREIGN KEY(CustomerID)
 REFERENCES Customer(CustomerID)
 	
---Practice Pipeline
-
+--Stored Procedure (Practice Pipeline)
 CREATE PROCEDURE Test
 AS
 BEGIN
-	--- Droping Temporary Table if Existed
+	-- Droping Temporary Table if Existed
 	DROP TABLE #TempTable
-	--- Creating Temporary Table
+	-- Creating Temporary Table
 	CREATE TABLE #TempTable(
 	Transaction_ID INT NOT NULL,
 	Order_ID NVARCHAR(50) NOT NULL,
